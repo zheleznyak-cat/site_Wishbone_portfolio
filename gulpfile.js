b3e8fcd1aa0c931cd25977b871d07333af6e458b
Ekaterina Zheleznyak <zheleznyak.cat@yandex.ru>
2025-03-16 22:50:42 +0300

    fourth commit

--- gulpfile.js
+++ gulpfile.js
@@ -12,6 +12,7 @@ gulp.task("server", function () {
     server: {
       baseDir: "dist",
     },
+    browser: "chrome", //чтобы открывался в googl chrom
   });
 
   gulp.watch("src/*.html").on("change", browserSync.reload);
